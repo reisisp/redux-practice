8423572e13cf648eeb19729de03a74044109045e
Alex <vuyanalex@gmail.com>
2022-12-12 00:41:15 +0300

    fix: small products model fix

--- src/entities/product/model/products.ts
+++ src/entities/product/model/products.ts
@@ -2,31 +2,12 @@ import { createSelector, createSlice } from "@reduxjs/toolkit";
 import { useSelector } from "react-redux";
 import { Product, ProductKeysTypeNumber } from "shared/api";
 
+import { tempArr } from "./temp";
+
 export const initialState: {
   data: Product[];
 } = {
-  data: [
-    {
-      id: "1",
-      status: "active",
-      sum: 555,
-      qty: 38,
-      name: "prod1",
-      delivery_date: "2020-05-12",
-      currency: "cur1",
-      volume: 3,
-    },
-    {
-      id: "2",
-      status: "archive",
-      sum: 333,
-      qty: 2,
-      name: "prod2",
-      delivery_date: "2020-05-11",
-      currency: "cur2",
-      volume: 7,
-    },
-  ],
+  data: tempArr,
 };
 
 export const productModel = createSlice({
