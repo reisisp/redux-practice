16c300c5287e8706d6aa252c063b903b9340be89
Alex <vuyanalex@gmail.com>
2022-12-11 18:08:29 +0300

    entity: small styles update ui/product-list

--- src/entities/product/ui/product-list/index.tsx
+++ src/entities/product/ui/product-list/index.tsx
@@ -3,12 +3,14 @@ import { productModel } from "entities/product/model";
 
 import { ProductCard } from "../product-card";
 
+import styles from "./index.module.scss";
+
 export const ProductList = () => {
   const { data } = productModel.getInitialState();
   return (
-    <List size="large" header={<div>header</div>} footer={<div>footer</div>} bordered>
+    <List size="large" header={<div>header</div>} footer={<div>footer</div>} bordered className={styles.list}>
       {data.map((el) => (
-        <List.Item key={el.id}>
+        <List.Item key={el.id} className={styles.list__item}>
           <ProductCard data={el} />
         </List.Item>
       ))}
