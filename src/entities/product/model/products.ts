fb6dab9124b8d6881d59d286259e4ad7e3439e3c
Alex <vuyanalex@gmail.com>
2022-12-11 16:43:09 +0300

    shared: update product type

--- src/entities/product/model/products.ts
+++ src/entities/product/model/products.ts
@@ -1,9 +1,31 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { Product } from "shared/api";
 
 export const initialState: {
-  data: { id: string }[];
+  data: Product[];
 } = {
-  data: [{ id: "1" }],
+  data: [
+    {
+      id: "1",
+      status: "active",
+      sum: 555,
+      qty: 38,
+      name: "prod1",
+      delivery_date: "20-11-2023",
+      currency: "cur1",
+      volume: 3,
+    },
+    {
+      id: "2",
+      status: "archive",
+      sum: 333,
+      qty: 2,
+      name: "prod2",
+      delivery_date: "27-11-2023",
+      currency: "cur2",
+      volume: 7,
+    },
+  ],
 };
 
 export const productModel = createSlice({
