c6d7f39f883ac8220b67c142071e34544687b17e
Alex <vuyanalex@gmail.com>
2022-12-11 17:56:29 +0300

    entity: update ui/product-card

--- src/entities/product/ui/product-card/index.tsx
+++ src/entities/product/ui/product-card/index.tsx
@@ -1,5 +1,24 @@
 import { FC } from "react";
+import { Col, Row } from "antd/es/grid";
+import { Product } from "shared/api";
 
-export const ProductCard: FC<{ data: any }> = ({ data }) => {
-  return <div>{data.id}</div>;
+import styles from "./index.module.scss";
+
+type ProductCardProps = {
+  data: Product;
+};
+
+export const ProductCard: FC<ProductCardProps> = ({ data }) => {
+  return (
+    <Row gutter={16} justify="space-between" className={styles.card}>
+      <Col span={2}>{data.name}</Col>
+      <Col span={2}>{data.status}</Col>
+      <Col span={2}>{data.delivery_date}</Col>
+      <Col span={2}>{data.currency}</Col>
+      <Col span={2}>{data.volume}</Col>
+      <Col span={2}>{data.qty}</Col>
+      <Col span={2}>{data.sum}</Col>
+      <Col span={2}>123</Col>
+    </Row>
+  );
 };
