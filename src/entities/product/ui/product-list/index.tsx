61ca8568cc2779516501aae6f65607ca6ee82f18
Alex <vuyanalex@gmail.com>
2022-12-11 21:20:38 +0300

    fix: add sorting to product-list

--- src/entities/product/ui/product-list/index.tsx
+++ src/entities/product/ui/product-list/index.tsx
@@ -7,9 +7,10 @@ import styles from "./index.module.scss";
 
 export const ProductList = () => {
   const data = useAllProducts();
+  const sorted = [...data].sort((a, b) => (Date.parse(a.delivery_date) > Date.parse(b.delivery_date) ? 1 : -1));
   return (
     <List size="large" header={<ProductListHeader />} footer={<ProductListFooter />} bordered className={styles.list}>
-      {data.map((el) => (
+      {sorted.map((el) => (
         <List.Item key={el.id} className={styles.list__item}>
           <ProductCard data={el} />
         </List.Item>
