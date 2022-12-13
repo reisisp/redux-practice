32799414b926c3ef0b3008f417f4a55dde313351
Alex <vuyanalex@gmail.com>
2022-12-14 02:34:20 +0300

    pages: fix routes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,18 @@ import { lazy } from "react";
 const ProductsPage = lazy(() => import("./products-page"));
 const NotFoundPage = lazy(() => import("./not-found-page"));
 
+const routes = [
+  { path: "/", element: <ProductsPage /> },
+  { path: "/not-found", element: <NotFoundPage /> },
+  { path: "*", element: <Navigate to="/not-found" /> },
+];
+
 export const Routing = () => {
   return (
     <Routes>
-      <Route path="/" element={<ProductsPage />} />
-      <Route path="/not-found" element={<NotFoundPage />} />
-      <Route path="*" element={<Navigate to="/not-found" />} />
+      {routes.map((el) => (
+        <Route key={el.path} path={el.path} element={el.element} />
+      ))}
     </Routes>
   );
 };
